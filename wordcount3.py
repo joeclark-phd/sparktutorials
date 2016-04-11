73db723df77cef738effac73a59727fac738b775
Joseph Clark <joseph.w.clark@asu.edu>
2016-04-11 16:30:11 -0700

    test v3

---
+++ wordcount3.py
@@ -0,0 +1,19 @@
+from pyspark import SparkConf, SparkContext
+
+conf = SparkConf().setMaster("local").setAppName("WordCount")
+sc = SparkContext(conf = conf)
+
+import re
+def normalizeWords(text):
+  return re.compile(r'\W+', re.UNICODE).split(text.lower())
+
+
+rddlocal = sc.textFile('file:///home/cloudera/sparktutorials/frost.txt')
+
+words = rddlocal.flatMap( normalizeWords )
+wordcounts = words.map(lambda x: (x,1)).reduceByKey(lambda x,y: x+y)
+
+print(wordcounts.collect())
+
+#for word, count in wordcounts.items():
+#  print("{0} {1}".format(word,count))
