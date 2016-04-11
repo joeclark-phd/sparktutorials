183c6da94e9725b254d1242bca98e8ae1117f878
Joseph Clark <joseph.w.clark@asu.edu>
2016-04-11 16:49:50 -0700

    wordcount4

--- wordcount4.py
+++ wordcount4.py
@@ -13,7 +13,11 @@ rddlocal = sc.textFile('file:///home/cloudera/sparktutorials/frost.txt')
 words = rddlocal.flatMap( normalizeWords )
 wordcounts = words.map(lambda x: (x,1)).reduceByKey(lambda x,y: x+y)
 
+
 wordcountsSorted = wordcounts.map(lambda (x,y):(y,x)).sortByKey()
+# python 3 version:
+# wordcountsSorted = wordcounts.map(lambda xy:(xy[1],xy[0])).sortByKey()
+
 
 for word, count in wordcountsSorted.collect():
   print("{0} {1}".format(word,count))
